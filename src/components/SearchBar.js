c63290f373e2e52737d2dfffe517bae785b06f45
Miguel Garvie <Miguelito@Doras-MacBook-Pro-2.local>
2025-01-14 14:08:07 +0700

    Better Search Bar Styling.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { TextInput, Button, View } from 'react-native';
+import { TextInput, Button, View, StyleSheet } from 'react-native';
 
 const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
   return (
-    <View>
+    <View style={styles.background}>
       <TextInput
+        style={styles.input}
         placeholder="Search News"
         value={term}
         onChangeText={onTermChange}
@@ -14,5 +15,20 @@ const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  background: {
+    margin: 15,
+    padding: 10,
+    backgroundColor: '#F0EEEE',
+    borderRadius: 5,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  input: {
+    flex: 1,
+    fontSize: 18
+  }
+});
 
 export default SearchBar;
